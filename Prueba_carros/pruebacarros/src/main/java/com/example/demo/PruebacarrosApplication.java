a432fa9f77aeea8ebfa5112c70940ead53b84371
Rojogenis <54334603+Rojogenis@users.noreply.github.com>
2019-08-20 17:21:13 -0500

    Correccion de bug

--- Prueba_carros/pruebacarros/src/main/java/com/example/demo/PruebacarrosApplication.java
+++ Prueba_carros/pruebacarros/src/main/java/com/example/demo/PruebacarrosApplication.java
@@ -8,7 +8,7 @@ public class PruebacarrosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebacarrosApplication.class, args);
-				System.out.println("Hola mundo desde git)";
+				System.out.println("Hola mundo desde git");
 
 		Camioneta coche1 = new Camioneta(6, 1500, 3500, 10, 15000);
 		System.out.println(coche1.toString());
